3e50ebabbdfda4963c7a14cd1b9428fbd02a620a
SportMode Code <james.epperson@kellogg.northwestern.edu>
2024-02-08 03:43:31 +0000

    class end

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,8 @@
 class CompaniesController < ApplicationController
 
+  # not needed, rails will figure it out, but the below is how it's done
   def index
      render :template => "companies/index"
   end
-  
+
 end
